a9a7b4cda7023942a2a6ed83c75c57e630ec83c0
Alejandro Andreu <alejandro@midokura.com>
2016-06-17 13:11:10 +0200

    Initial version of controller node
    Installs:
     - mysql
     - rabbitmq
     - keystone
     - glance
    
    Change-Id: I5165c1d79471bfb60ea802d2d16fcd7e52321ddc

--- manifests/role/controller.pp
+++ manifests/role/controller.pp
@@ -14,24 +14,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-class midonet_openstack::role::controller inherits ::openstack::role {
+class midonet_openstack::role::controller {
 
-  class { '::openstack::profile::firewall': }
-  class { '::openstack::profile::rabbitmq': } ->
-  class { '::openstack::profile::mysql': } ->
-  class { '::openstack::profile::keystone': } ->
-  class { '::midonet_openstack::profile::neutron::server':} ->
-  class { '::midonet_openstack::profile::neutron::router':} ->
-  class { '::openstack::profile::glance::api': } ->
-  class { '::openstack::profile::glance::auth': } ->
-  class { '::openstack::profile::cinder::api': } ->
-  class { '::openstack::profile::cinder::volume': } ->
-  class { '::openstack::profile::nova::api': } ->
-  class { '::openstack::profile::horizon': }
-  class { '::openstack::profile::auth_file': }
-  class { '::midonet_openstack::setup::sharednetwork': }
-  class { '::openstack::setup::cirros': }
+  # ****************************************************************************
+  # Old Puppet modules, exposed here as reference
+  # ****************************************************************************
 
-  Class['::midonet_openstack::setup::sharednetwork'] -> Class['::midonet_openstack::profile::neutron::server']
-  Class['::midonet_openstack::setup::sharednetwork'] -> Class['::apache::service']
+  #class { '::openstack::profile::firewall': }
+  #class { '::openstack::profile::rabbitmq': } ->
+  #class { '::openstack::profile::mysql': } ->
+  #class { '::openstack::profile::keystone': } ->
+  #class { '::midonet_openstack::profile::neutron::server':} ->
+  #class { '::midonet_openstack::profile::neutron::router':} ->
+  #class { '::openstack::profile::glance::api': } ->
+  #class { '::openstack::profile::glance::auth': } ->
+  #class { '::openstack::profile::cinder::api': } ->
+  #class { '::openstack::profile::cinder::volume': } ->
+  #class { '::openstack::profile::nova::api': } ->
+  #class { '::openstack::profile::horizon': }
+  #class { '::openstack::profile::auth_file': }
+  #class { '::midonet_openstack::setup::sharednetwork': }
+  #class { '::openstack::setup::cirros': }
+  #Class['::midonet_openstack::setup::sharednetwork'] ->
+  #  Class['::midonet_openstack::profile::neutron::server']
+  #Class['::midonet_openstack::setup::sharednetwork'] ->
+  #  Class['::apache::service']
+
+
+  # ****************************************************************************
+  # New stuff
+  # ****************************************************************************
+
+  include ::openstack_integration
+  include ::openstack_integration::rabbitmq
+  include ::openstack_integration::mysql
+  include ::openstack_integration::keystone
+
+  # Create necessary users in RabbitMQ and grant them permissions
+  rabbitmq_user { ['neutron', 'nova', 'glance']:
+    admin    => true,
+    password => 'an_even_bigger_secret',
+    provider => 'rabbitmqctl',
+    require  => Class['::rabbitmq'],
+  }
+  rabbitmq_user_permissions { ['neutron@/', 'nova@/', 'glance@/']:
+    configure_permission => '.*',
+    write_permission     => '.*',
+    read_permission      => '.*',
+    provider             => 'rabbitmqctl',
+    require              => Class['::rabbitmq'],
+  }
+
 }
