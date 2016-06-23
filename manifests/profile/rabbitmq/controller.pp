ea21ebd524bb7c247a3818833e8ff9d73e22f6bd
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-06-23 11:29:02 +0200

    Add fix for Rabbitmq on Ubuntu
    Change-Id: Id197ae8454ce8a80fbe5d4267d83bf79826000e4

--- manifests/profile/rabbitmq/controller.pp
+++ manifests/profile/rabbitmq/controller.pp
@@ -3,7 +3,24 @@ class midonet_openstack::profile::rabbitmq::controller {
   include ::openstack_integration::params
   include ::openstack_integration::config
 
+  ## Dirty workaround until the guys from puppetlabs make new release
+  ## with correct apt key for rabbitmq..
+  if ($::osfamily == 'Debian') or ($::osfamily == 'Debian')
+  {
+    apt::key { 'rabbitmq':
+      key          => '0A9AF2115F4687BD29803A206B73A36E6026DFCA',
+      key_source   => 'https://www.rabbitmq.com/rabbitmq-release-signing-key.asc',
+      before => Class['::rabbitmq']
+      } ->
+      exec { 'update_apt_for_rabbitmq':
+          command     => "/usr/bin/apt-get update",
+          logoutput   => 'on_failure',
+        }
+  }
+
   if $::openstack_integration::config::ssl {
+
+
     file { '/etc/rabbitmq/ssl/private':
       ensure                  => directory,
       owner                   => 'root',
@@ -32,6 +49,7 @@ class midonet_openstack::profile::rabbitmq::controller {
       package_provider      => $::package_provider,
       delete_guest_user     => $midonet_openstack::params::rabbitmq_delete_guest_user,
       environment_variables => $::openstack_integration::config::rabbit_env,
+      repos_ensure          => $midonet_openstack::params::rabbitmq_repos_ensure,
     }
   }
   rabbitmq_vhost { '/':
