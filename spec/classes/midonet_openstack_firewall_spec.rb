1fa5457a2e8ebdf0e69746aa89e0545550f90159
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-06-29 09:59:10 +0200

    Add the first rspec test
    Change-Id: I8708a5022ed6da40cfe8a606f70af459ab268753

---
+++ spec/classes/midonet_openstack_firewall_spec.rb
@@ -0,0 +1,58 @@
+
+#
+# Unit tests for midonet_openstack::profile::firewall::firewall
+#
+
+require 'spec_helper'
+
+describe 'midonet_openstack::profile::firewall::firewall' do
+
+  let :default_params do
+    { }
+  end
+
+  shared_examples_for 'neutron firewall' do
+
+    context 'with default params' do
+      let :params do
+        default_params
+      end
+
+      it 'should contain firewall pre and post classes' do
+        is_expected.to contain_class('midonet_openstack::profile::firewall::pre')
+        is_expected.to contain_class('midonet_openstack::profile::firewall::post')
+      end
+  end
+end
+
+  context 'on Debian platforms' do
+    let :facts do
+      @default_facts.merge({
+        :osfamily  => 'Debian',
+        :kernel => 'Linux'
+      })
+    end
+
+    let :platform_params do
+      {}
+    end
+
+    it_configures 'neutron firewall'
+  end
+
+  context 'on Red Hat platforms' do
+    let :facts do
+      @default_facts.merge({
+        :osfamily => 'RedHat',
+        :operatingsystemrelease => '7',
+        :kernel => 'Linux'
+      })
+    end
+
+    let :platform_params do
+      {}
+    end
+
+    it_configures 'neutron firewall'
+  end
+end
