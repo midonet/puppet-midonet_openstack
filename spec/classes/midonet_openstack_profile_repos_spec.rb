4989e8e47d68e3aa3fec8dee3aa310bc8013f5eb
Alejandro Andreu <alejandro@midokura.com>
2016-06-29 13:17:17 +0200

    Adds test for midonet_openstack::profile::repos

---
+++ spec/classes/midonet_openstack_profile_repos_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe 'midonet_openstack::profile::repos' do
+
+  let :default_params do
+    { }
+  end
+
+  context 'on Ubuntu 14.04 LTS' do
+    let :facts do
+      @default_facts.merge({
+        :osfamily  => 'Debian',
+        :kernel => 'Linux',
+        :lsbdistid => 'Ubuntu',
+        :lsbdistcodename => 'Trusty'
+      })
+    end
+
+    it do
+      is_expected.to contain_class('openstack_extras::repo::debian::ubuntu').with(
+      'release' => 'mitaka',
+      'package_require' => 'true',
+      )
+      is_expected.to contain_apt__pin('ceph').with(
+      'priority' => '1001',
+      'origin' => 'download.ceph.com'
+      )
+    end
+  end
+
+  context 'on RHEL based platforms' do
+    let :facts do
+      @default_facts.merge({
+        :osfamily => 'RedHat',
+        :operatingsystemrelease => '7',
+        :kernel => 'Linux',
+        :puppetversion => '3.8.7'
+      })
+    end
+
+    it do
+      is_expected.to contain_class('openstack_extras::repo::redhat::redhat').with(
+      'release' => 'mitaka',
+      'manage_epel' => 'false',
+      'centos_mirror_url' => 'http://mirror.centos.org',
+      'manage_priorities' => 'false'
+      )
+      is_expected.to contain_class('ceph::repo')
+    end
+  end
+end
