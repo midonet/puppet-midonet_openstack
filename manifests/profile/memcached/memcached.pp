c24679fbb3a469383716617e084188645d570f08
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-07-04 18:22:01 +0200

    Rename memcached
    Change-Id: Ie06f6e0dcb59487554bc1ab69e02042108a8e785

--- manifests/profile/memcached/memcached.pp
+++
@@ -1,8 +0,0 @@
-# The profile to install a local instance of memcache
-class midonet_openstack::profile::memcache::memcache($port = '11211') {
-  class { 'memcached':
-    listen_ip => $::midonet_openstack::params::controller_address_management, #'127.0.0.1',
-    tcp_port  => $port,
-    udp_port  => $port,
-  }
-}
