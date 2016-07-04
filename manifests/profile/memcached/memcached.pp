2b07afeb21041200e5b2a315609fbec09a313d7e
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-07-04 15:46:46 +0200

    Add port parameter to memcached profile
    Change-Id: I0bdb67c15ad2a7578c0415f4883cb477a950c7b0

--- manifests/profile/memcached/memcached.pp
+++ manifests/profile/memcached/memcached.pp
@@ -1,8 +1,8 @@
 # The profile to install a local instance of memcache
-class midonet_openstack::profile::memcached::memcached {
+class midonet_openstack::profile::memcached::memcached($port = '11211') {
   class { 'memcached':
     listen_ip => $::midonet_openstack::params::controller_address_management, #'127.0.0.1',
-    tcp_port  => '11211',
-    udp_port  => '11211',
+    tcp_port  => $port,
+    udp_port  => $port,
   }
 }
