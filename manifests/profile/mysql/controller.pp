4042f658600d1128af17812575eeef22d9c00df8
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-07-25 18:32:56 +0200

    Fix Mysql Controller Binding IP
    Change-Id: I7ff6f483c60950ed6c5396f754af5a551857779e

--- manifests/profile/mysql/controller.pp
+++ manifests/profile/mysql/controller.pp
@@ -2,5 +2,10 @@
 #
 #  Configure MySQL on a controller node
 class midonet_openstack::profile::mysql::controller {
-  class { '::mysql::server': }
+  class { '::mysql::server':
+    override_options => {
+      mysqld => { 'bind-address' => $::midonet_openstack::params::controller_address_management} #Allow remote connections
+      }
+    }
+
 }
