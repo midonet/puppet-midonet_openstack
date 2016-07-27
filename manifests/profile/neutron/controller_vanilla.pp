02197f3e6187f4858a460cf3ea2513547cd00a97
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-07-27 12:35:42 +0200

    Modify IPs on roles
    Change-Id: I1dfd9a53b380f86bf89906af2cb58211c054ca5e

--- manifests/profile/neutron/controller_vanilla.pp
+++ manifests/profile/neutron/controller_vanilla.pp
@@ -30,8 +30,10 @@ class midonet_openstack::profile::neutron::controller_vanilla {
     allowed_hosts => '%',
   }
   class { '::neutron::keystone::auth':
-    password => $::midonet_openstack::params::neutron_password,
-    region   => $::midonet_openstack::params::region
+    password   => $::midonet_openstack::params::neutron_password,
+    region     => $::midonet_openstack::params::region,
+    public_url => $controller_api_address
+    admin_url  => $controller_management_address
   }
 
   class { '::neutron':
