93e951f91b97be270793977d93b655a666a54932
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-08-16 18:43:08 +0200

    Add firewall rule for port 8181
    Change-Id: Ic2adab2fbe75dc36c1e0684b6b1785c5cc1ee866

--- manifests/role/allinone.pp
+++ manifests/role/allinone.pp
@@ -132,6 +132,8 @@ class midonet_openstack::role::allinone (
     File_line<| match == 'libvirtd_opts='  |> { line => 'libvirtd_opts="-l"' }
   }
 
+  midonet_openstack::resources::firewall { 'Midonet API': port => '8181', }
+
   # Register the host
   midonet_host_registry { $::fqdn:
     ensure          => present,
