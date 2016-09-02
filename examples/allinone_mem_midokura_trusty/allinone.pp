976e6c2015c5a8309528d09df29c999fd9eb5439
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-09-02 09:59:08 +0200

    Fix the aio
    Change-Id: I39a91d3c2a91dabc990bb6720ca2cbb6bd03938b

--- examples/allinone_mem_midokura_trusty/allinone.pp
+++ examples/allinone_mem_midokura_trusty/allinone.pp
@@ -1,6 +1,6 @@
 class { 'midonet_openstack::role::allinone_mem':
-        mem_username          => 'qa',
-        mem_password          => 'gogoqat3am',
+        mem_username          => '',
+        mem_password          => '',
         mem_apache_servername => 'marioneta.midonet.org',
         horizon_extra_aliases => ['horizonte.midonet.org'],
         cluster_ip            => generate('/bin/sh', '-c', '/usr/bin/curl -s ifcfg.me | tr -d "\n"'),
