8d5934d71cde83f6999bdc1ba3cba3d78194fadb
sandip-calsoft <sandip.divekar@calsoftinc.com>
2016-09-19 06:09:40 +0000

    adding changes for insights and mem integration
    Change-Id: I75d1355411c7c68677e545e170aa6f09d0d60950

--- examples/allinone_mem_midokura_xenial/allinone.pp
+++ examples/allinone_mem_midokura_xenial/allinone.pp
@@ -6,4 +6,6 @@ class { 'midonet_openstack::role::allinone_mem':
         cluster_ip            => generate('/bin/sh', '-c', '/usr/bin/curl -s ifcfg.me | tr -d "\n"'),
         is_insights           => undef,
         analytics_ip          => '',
+        is_ssl                => false,
+        insights_ssl          => false,
 }
