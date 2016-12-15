469e59d28a37b2ec7616339858faec5aba03f09c
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-12-15 13:14:25 +0100

    Fix AIO Manifests
    Change-Id: I8a46e07f7cdbee1bbf7305a279b4f8586e2b5dc4

--- examples/allinone_mem_midokura_xenial/allinone.pp
+++ examples/allinone_mem_midokura_xenial/allinone.pp
@@ -3,7 +3,7 @@ class { 'midonet_openstack::role::allinone_mem':
         mem_password          => '',
         mem_apache_servername => '',
         horizon_extra_aliases => ['horizonte.midonet.org'],
-        cluster_ip            => generate('/bin/sh', '-c', '/usr/bin/curl -s ifcfg.me | tr -d "\n"'),
+        cluster_ip            => generate('/bin/sh', '-c', '/usr/bin/curl -s ifconfig.io | tr -d "\n"'),
         is_insights           => undef,
         analytics_ip          => '',
         is_ssl                => false,
